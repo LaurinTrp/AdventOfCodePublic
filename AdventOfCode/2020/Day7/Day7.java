3ee6ffcdffc28a5e9dd7cec0c54ddaa5a1a510bf
lauri <lauri@LAPTOP-8IC3PK6Q.fritz.box>
2020-12-07 14:30:31 +0100

    Day 7

---
+++ AdventOfCode/2020/Day7/Day7.java
@@ -0,0 +1,12 @@
+package Day7;
+
+import java.util.ArrayList;
+
+import Globals.FileReader;
+
+public class Day7 {
+	public static void main(String[] args) {
+		ArrayList<String> strings = FileReader.getFileContent("Day7/Bags");
+		System.out.println(strings);
+	}
+}
